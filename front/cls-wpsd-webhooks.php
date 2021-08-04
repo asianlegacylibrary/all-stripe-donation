fcccc6d65f96ef192aba7aed3d610b462087a5fd
joelcrawford <joelrootcrawford@gmail.com>
2021-08-04 15:36:37 -0700

    updating data sent to kindful

--- front/cls-wpsd-webhooks.php
+++ front/cls-wpsd-webhooks.php
@@ -117,9 +117,9 @@ class Wpsd_Webhooks {
 		$customer_id = $paymentIntent->charges->data[0]->customer;
 		if(isset($customer_id) || !trim($customer_id) === '') {
 			$customer = $this->wpsd_get_stripe_customer_by_id($customer_id);
-			$metadata = $this->client->customers->retrieve($customer->metadata);
+			$metadata = $this->client->customers->retrieve($customer);
 			//$metadata = $customer->metadata->_toArray();
-			echo var_dump('METADATA straight up', empty((array)$metadata), count((array)$metadata), !(array)$metadata, $metadata == new stdClass());
+			echo var_dump('METADATA straight up', empty((array)$metadata->metadata), count((array)$metadata->metadata), !(array)$metadata->metadata, $metadata->metadata == new stdClass());
 			$metadata_array = array(
 				'campaign' => $metadata->campaign,
 				'campaign_id' => $metadata->campaign_id,
